f10b860879499308a13cf31cf070cd70cb73e9f6
fummicc1 <fumiya.tennis1@gmail.com>
2022-06-17 00:18:41 +0900

    Initial Commit

---
+++ SecretCount/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecretCount
+//
+//  Created by Fumiya Tanaka on 2022/06/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
